f92e9cdb36fb1215ccc198604cc10e2c60bb7f0d
phuonglamit <ptranphuong.it@gmail.com>
2025-03-04 23:26:02 +0700

    D03E08 - Update dieu kien cua nam nhuan

--- src/D03E08Ex.java
+++ src/D03E08Ex.java
@@ -4,7 +4,7 @@ public class D03E08Ex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
-        if (year % 4 == 0 && year % 100 != 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Day la nam nhuan: " + year);
 
         } else {
